4d4c889986c29adc8b97fb0d993c5ea280234e32
mo <meganannetteolson@yahoo.com>
2017-10-20 15:14:58 -0700

    display full length of array for medical issue search

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -18,8 +18,9 @@ export class DoctorLookup {
   byMedicalIssue(medicalIssue){
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=47.185%2C-122.292%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=16dc9d6f9af205b02a71b04cacdc7348`)
     .then(function(response){
+      console.log(response.data.length);
       for (var i = 0; i < response.data.length; i++) {
-  			$('#doctorResultsByIssue').append(`<li>Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br> Address: ${response.data[i].practices[0].visit_address.city},${response.data[i].practices[0].visit_address.state} <br>Phone Number: ${response.data[i].practices[1].phones[0].number}<br>Accepting New Patients: ${response.data[i].practices[0].accepts_new_patients}<br>Website: ${response.data[i].practices[0].website}</li>`);
+  			$('#doctorResultsByIssue').append(`<li>Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br> Address: ${response.data[i].practices[0].visit_address.city},${response.data[i].practices[0].visit_address.state} <br>Phone Number: ${response.data[i].practices[0].phones[0].number}<br>Accepting New Patients: ${response.data[i].practices[0].accepts_new_patients}<br>Website: ${response.data[i].practices[0].website}</li>`);
 		   }
     }).fail(function(error){
       $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
