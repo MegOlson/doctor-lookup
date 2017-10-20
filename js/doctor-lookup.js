2845026ecd7139ae656446251bbd54cbd8a17512
mo <meganannetteolson@yahoo.com>
2017-10-20 15:25:28 -0700

    add display message when there are no matching results

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -7,6 +7,9 @@ export class DoctorLookup {
   byLastName(lastName){
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=47.185%2C-122.292%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=16dc9d6f9af205b02a71b04cacdc7348`)
     .then(function(response){
+      if (response.data.length === 0) {
+        $('#doctorResultsByIssue').append("There are no doctor's in this area that match with your search.")
+      }
       for (var i = 0; i < response.data.length; i++) {
   			$('#doctorResultsByName').append(`<li>Name: ${response.data[i].practices[0].name}<br> Address: ${response.data[i].practices[0].visit_address.city},${response.data[i].practices[0].visit_address.state} <br>Phone Number: ${response.data[i].practices[0].phones[0].number}<br> Website: ${response.data[i].practices[0].website}<br>Accepting New Patients: ${response.data[1].practices[0].accepts_new_patients}</li>`);
 		   }
@@ -18,7 +21,9 @@ export class DoctorLookup {
   byMedicalIssue(medicalIssue){
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=47.185%2C-122.292%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=16dc9d6f9af205b02a71b04cacdc7348`)
     .then(function(response){
-      console.log(response.data.length);
+      if (response.data.length === 0) {
+        $('#doctorResultsByIssue').append("There are no doctor's in this area that match with your search.")
+      }
       for (var i = 0; i < response.data.length; i++) {
   			$('#doctorResultsByIssue').append(`<li>Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}<br> Address: ${response.data[i].practices[0].visit_address.city},${response.data[i].practices[0].visit_address.state} <br>Phone Number: ${response.data[i].practices[0].phones[0].number}<br>Accepting New Patients: ${response.data[i].practices[0].accepts_new_patients}<br>Website: ${response.data[i].practices[0].website}</li>`);
 		   }
