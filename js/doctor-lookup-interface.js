d1ef2e959cb9253273684ecbaf9b4be638356124
mo <meganannetteolson@yahoo.com>
2017-10-20 16:29:08 -0700

    add empty to both forms on reloading results

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -3,6 +3,7 @@ import { DoctorLookup } from './../js/doctor-lookup.js';
 $(document).ready(function(){
   let doctorLookup = new DoctorLookup();
 
+  // Following two functions create the loading GIF while ajax request completes
   $(document).ajaxStart(function(){
     $("#wait").css("display", "block");
   });
@@ -14,12 +15,14 @@ $(document).ready(function(){
 
   $(".searchByName").submit(function(e){
     e.preventDefault();
+    $("ul#doctorResultsByName").empty();
     const lastName = $("input.searchByName").val();
     doctorLookup.byLastName(lastName);
     $(".resultsByName").removeClass("hide");
   });
   $(".searchByIssue").submit(function(e){
     e.preventDefault();
+    $("ul#doctorResultsByIssue").empty();
     const medicalIssue = $("input.searchByIssue").val();
     doctorLookup.byMedicalIssue(medicalIssue);
     $(".resultsByIssue").removeClass("hide");
